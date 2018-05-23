d3c0ed9f7a3d88428dc5ab754c50559ac604fa20
unknown <cpp8013@hikyjy.com.cn>
2018-05-23 16:35:17 +0800

    同步20180523

--- schemtic/sch/temp/packagerxl.bat
+++ schemtic/sch/temp/packagerxl.bat
@@ -1,4 +1,4 @@
 echo off
 netassembler
-concept2cm -forward -export -proj "E:\GitHub\KVM_prototype\schemtic\sch\kvm_prototype.cpm"
-pxl.exe -proj "E:\GitHub\KVM_prototype\schemtic\sch\kvm_prototype.cpm" -nonetassembler
+concept2cm -forward -export -proj "F:\Design_Projects\matrix_kvm\schemtic_backup20180518\sch\kvm_prototype.cpm"
+pxl.exe -proj "F:\Design_Projects\matrix_kvm\schemtic_backup20180518\sch\kvm_prototype.cpm" -nonetassembler
